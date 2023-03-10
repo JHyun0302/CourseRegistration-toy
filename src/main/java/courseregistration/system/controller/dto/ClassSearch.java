dd032c919ae391de6ec7c5187bf01d9c75b5ae4f
jaehyun <wogustkfkd11@gmail.com>
2023-03-10 22:39:52 +0900

    ClassSearch DTO

---
+++ src/main/java/courseregistration/system/controller/dto/ClassSearch.java
@@ -0,0 +1,11 @@
+package courseregistration.system.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClassSearch {
+    private Long majorId;
+    private Long courseId;
+}
