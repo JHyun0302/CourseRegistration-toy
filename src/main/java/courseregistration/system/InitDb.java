b7874b8e627f6e1637809766223118c7c7572646
jaehyun <wogustkfkd11@gmail.com>
2023-03-12 23:24:53 +0900

    init data 버그 수정: dto -> user

--- src/main/java/courseregistration/system/InitDb.java
+++ src/main/java/courseregistration/system/InitDb.java
@@ -1,96 +1,86 @@
 package courseregistration.system;
 
-import courseregistration.system.controller.dto.init.*;
-import courseregistration.system.entity.Role;
+import courseregistration.system.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
 
 @Component
-@Transactional
 @RequiredArgsConstructor
 public class InitDb {
-    private final EntityManager em;
 
-    public void dbInit1() {
-        MajorDto major = createMajor("software");
-        em.persist(major);
+    private final InitService initService;
 
-        UserDto user = createUser("test1", "test2!", "userA", major, Role.STUDENT, "userA@xxx.com", "010-1234-5678");
-        em.persist(user);
-
-        CourseDto cLanguage = createCourse(major, "C Programming");
-        em.persist(cLanguage);
-
-        ClassesDto classes1 = createClasses(cLanguage, 1, "lee", 50, 0);
-        em.persist(classes1);
-
-        TakeClassesDto takeClass = createTakeClass(user, classes1);
-        em.persist(takeClass);
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
     }
 
-    public void dbInit2() {
-        MajorDto major = createMajor("computerScience");
-        em.persist(major);
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
 
-        UserDto user = createUser("test2", "test2!", "userB", major, Role.STUDENT, "userB@xxx.com", "010-2222-5678");
-        em.persist(user);
+        public void dbInit1() {
+            Major major = createMajor("software");
+            em.persist(major);
 
-        CourseDto javaLanguage = createCourse(major, "Java Programming");
-        em.persist(javaLanguage);
+            User user = createUser("test1", "test1!", "userA", major, Role.STUDENT, "userA@xxx.com", "010-1234-5678");
+            em.persist(user);
 
-        ClassesDto classes2 = createClasses(javaLanguage, 2, "kim", 30, 0);
-        em.persist(classes2);
+            Course cLanguage = createCourse(major, "C Programming");
+            em.persist(cLanguage);
 
-        TakeClassesDto takeClass = createTakeClass(user, classes2);
-        em.persist(takeClass);
+            Classes classes1 = createClasses(cLanguage, 1, "lee", 50, 0);
+            em.persist(classes1);
+
+            TakeClass takeClass = createTakeClass(user, classes1);
+            em.persist(takeClass);
+        }
+
+        public void dbInit2() {
+            Major major = createMajor("computerScience");
+            em.persist(major);
+
+            User user = createUser("test2", "test2!", "userB", major, Role.STUDENT, "userB@xxx.com", "010-2222-5678");
+            em.persist(user);
+
+            Course javaLanguage = createCourse(major, "Java Programming");
+            em.persist(javaLanguage);
+
+            Classes classes2 = createClasses(javaLanguage, 2, "kim", 30, 0);
+            em.persist(classes2);
+
+            TakeClass takeClass = createTakeClass(user, classes2);
+            em.persist(takeClass);
+
+        }
+
+        public static TakeClass createTakeClass(User user, Classes classes) {
+            return new TakeClass(user, classes);
+        }
+
+        public static Classes createClasses(Course course, int classNumber, String professorName, int maxStudentNum, int curStudentNum) {
+            return new Classes(course, classNumber, professorName, maxStudentNum, curStudentNum);
+        }
+
+        public static Course createCourse(Major major, String courseName) {
+            return new Course(major, courseName);
+        }
+
+        public static User createUser(String loginId, String password, String username, Major major, Role role, String email, String phoneNumber) {
+            return new User(loginId, password, username, major, role, email, phoneNumber);
+        }
+
+        public static Major createMajor(String majorName) {
+            return new Major(majorName);
+        }
     }
-
-    private static TakeClassesDto createTakeClass(UserDto user, ClassesDto classes) {
-        TakeClassesDto takeClasses = new TakeClassesDto(user, classes);
-        takeClasses.setUser(user);
-        takeClasses.setClasses(classes);
-
-        return takeClasses;
-    }
-
-
-    private static ClassesDto createClasses(CourseDto cLanguage, int classNumber, String professorName, int maxStudentNum, int curStudentNum) {
-        ClassesDto classes = new ClassesDto();
-        classes.setCourse(cLanguage);
-        classes.setClassNumber(classNumber);
-        classes.setProfessorName(professorName);
-        classes.setMaxStudentNum(maxStudentNum);
-        classes.setCurStudentNum(curStudentNum);
-        return classes;
-    }
-
-    private static CourseDto createCourse(MajorDto major, String subjectName) {
-        CourseDto course = new CourseDto();
-        course.setMajor(major);
-        course.setCourseName(subjectName);
-        return course;
-    }
-
-    private static UserDto createUser(String loginId, String password, String username, MajorDto major, Role role, String email, String phoneNumber) {
-        UserDto user = new UserDto();
-        user.setLoginId(loginId);
-        user.setPassword(password);
-        user.setUsername(username);
-        user.setMajor(major);
-        user.setRole(role);
-        user.setEmail(email);
-        user.setPhoneNumber(phoneNumber);
-        return user;
-    }
-
-    private static MajorDto createMajor(String major) {
-        MajorDto majorDto = new MajorDto();
-        majorDto.setMajorName(major);
-        return majorDto;
-    }
-
 }
+
