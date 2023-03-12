09efc846f06abffe6a13b72c6f783fb16ee42078
jaehyun <wogustkfkd11@gmail.com>
2023-03-12 23:25:07 +0900

    오타 수정

--- src/main/java/courseregistration/system/interceptor/LoginCheckInterceptor.java
+++ src/main/java/courseregistration/system/interceptor/LoginCheckInterceptor.java
@@ -19,7 +19,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
             log.info("미인증 사용자 요청");
 
-            response.sendRedirect("/log?redirectURL=" + requestURI);
+            response.sendRedirect("/login?redirectURL=" + requestURI);
             return false;
         }
         return true;
