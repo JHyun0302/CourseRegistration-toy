f6f5d9a0388bde234b4f4a2697ce544699c724d2
jaehyun <wogustkfkd11@gmail.com>
2023-03-14 22:40:51 +0900

    수정 중 continue

--- src/main/java/courseregistration/system/controller/UserController.java
+++ src/main/java/courseregistration/system/controller/UserController.java
@@ -6,17 +6,19 @@ import courseregistration.system.controller.dto.UserUpdateRequestDto;
 import courseregistration.system.entity.Major;
 import courseregistration.system.service.MajorService;
 import courseregistration.system.service.UserService;
+import courseregistration.system.service.exception.LoginIdException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -30,54 +32,61 @@ public class UserController {
     public String signup(@ModelAttribute("signUpDto") UserSignUpDto signUpDto, Model model) {
         List<Major> majors = majorService.findAll();
 
+        model.addAttribute("signUpDto", signUpDto);
         model.addAttribute("majors", majors);
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String saveUser(@Valid @ModelAttribute UserSignUpDto userSignUpDto, Model model, BindingResult bindingResult) {
-        userService.join(userSignUpDto);
-
-        if (bindingResult.hasErrors()) {
-            return "signup";
-        }
-
+    public String saveUser(@Validated @ModelAttribute("signUpDto") UserSignUpDto signUpDto, BindingResult bindingResult, Model model) {
         List<Major> majors = majorService.findAll();
         model.addAttribute("majors", majors);
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "/signup";
+        }
+
+        try {
+            Long savedUser = userService.join(signUpDto);
+        } catch (LoginIdException e) {
+            bindingResult.reject("PW & PW_confirm Not the same.", "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+            return "/signup";
+        }
         return "redirect:/";
-    }
-
-
-    @GetMapping("/myCourses/{username}")
-    public String myCourseList(@PathVariable String username, Model model) {
-        UserResponseDto userResponseDto = userService.findByUsername(username);
-
-        model.addAttribute("username", userResponseDto.getUsername());
-        model.addAttribute("takeClasses", userResponseDto.getTakeClasses());
-
-        return "myCourseList";
     }
 
     @GetMapping("/edit/{username}")
-    public String update(@PathVariable String username, Model model) {
-        UserResponseDto userResponseDto = userService.findByUsername(username);
+    public String userInfo(@PathVariable String username, Model model) {
+        UserResponseDto userResponseDto = userService.findDtoByUsername(username);
 
         model.addAttribute("userUpdateRequestDto", new UserUpdateRequestDto(userResponseDto));
-        return "edit";
+        return "edit/userInfo";
     }
 
 
-    @PostMapping("/edit/{username}")
-    public String userInfoUpdate(@PathVariable String username, @ModelAttribute UserUpdateRequestDto userUpdateRequestDto, Model model, BindingResult bindingResult) {
-        UserResponseDto userResponseDto = userService.findByUsername(username);
+    @GetMapping("/edit/{username}/edit")
+    public String update(@PathVariable String username, Model model) {
+        UserResponseDto userResponseDto = userService.findDtoByUsername(username);
 
+        model.addAttribute("userUpdateRequestDto", new UserUpdateRequestDto(userResponseDto));
+        return "edit/editForm";
+    }
+
+
+    @PostMapping("/edit/{username}/edit")
+    public String userInfoUpdate(@PathVariable String username, @Validated @ModelAttribute("userUpdateRequestDto") UserUpdateRequestDto userUpdateRequestDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        UserResponseDto userResponseDto = userService.findDtoByUsername(username);
+
+        if (bindingResult.hasErrors()) {
+            log.info("error= {}", bindingResult);
+            return "edit/editForm";
+        }
+        model.addAttribute("userUpdateRequestDto", userUpdateRequestDto);
         userService.update(userResponseDto.getUserId(), userUpdateRequestDto);
 
-        model.addAttribute("userUpdateRequestDto", userUpdateRequestDto);
-        if (bindingResult.hasErrors()) {
-            return "edit";
-        }
-
-        return "redirect:/";
+        redirectAttributes.addAttribute("username", username);
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/edit/{username}";
     }
 }
