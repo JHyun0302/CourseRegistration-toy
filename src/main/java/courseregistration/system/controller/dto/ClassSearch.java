a4f4b7fcb42bfa10ac5ffbf90e9327c891fbdc06
jaehyun <wogustkfkd11@gmail.com>
2023-03-14 22:40:10 +0900

    기본 생성자

--- src/main/java/courseregistration/system/controller/dto/ClassSearch.java
+++ src/main/java/courseregistration/system/controller/dto/ClassSearch.java
@@ -1,11 +1,19 @@
 package courseregistration.system.controller.dto;
 
+import courseregistration.system.entity.Course;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ClassSearch {
     private Long majorId;
     private Long courseId;
+
+    public ClassSearch(Course course) {
+        this.majorId = majorId;
+        this.courseId = courseId;
+    }
 }
