3b76bbfb012fb5d54d5e1266efc1af8077653777
jaehyun <wogustkfkd11@gmail.com>
2023-03-18 15:30:16 +0900

    폴더 리팩토링

--- src/main/java/courseregistration/system/service/MajorService.java
+++ src/main/java/courseregistration/system/service/MajorService.java
@@ -1,8 +1,8 @@
 package courseregistration.system.service;
 
 import courseregistration.system.entity.Major;
+import courseregistration.system.exception.ResourceNotFoundException;
 import courseregistration.system.repository.MajorRepository;
-import courseregistration.system.service.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
